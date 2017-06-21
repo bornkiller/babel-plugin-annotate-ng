196c4af3f872ab5ecd73fff571af38e7b09786db
黄健 <huangjian@huangjiandeMacBook-Pro.local>
2017-06-21 15:50:23 +0800

    feature: initial comment inject.

--- src/index.js
+++ src/index.js
@@ -7,26 +7,58 @@
 /**
  * @description - determine whether function declare has @ngInject commend
  *
- * @param {Object} node
+ * @param {Object} path
+ * @param types
  *
  * @return {*}
  */
-function inspectAnnotationComment(node) {
+function inspectAnnotationExports(path, types) {
   // skip none-comment function declare
-  if (!node.leadingComments) return null;
+  if (!path.node.leadingComments) return null;
 
-  return node.leadingComments
+  return path.node.leadingComments
     .map((comment) => comment.value.trim())
     .find((comment) => comment === '@ngInject');
 }
 
-module.exports = function (babel) {
+function annotateFunctionDeclare(path, types) {
+  const { id, params } = path.node.declaration;
+
+  path.node.leadingComments = null;
+  path.insertBefore(types.expressionStatement(types.assignmentExpression(
+    '=',
+    types.MemberExpression(id, types.identifier('$inject')),
+    types.arrayExpression(params.map((identifier) => types.stringLiteral(identifier.name)))
+  )));
+}
+
+function annotateClassDeclare(path, types) {
+  const declaration = path.node.declaration;
+  const params = declaration.body.body.find((ClassMethod) => ClassMethod.kind === 'constructor').params;
+
+  path.node.leadingComments = null;
+  path.insertBefore(types.expressionStatement(types.assignmentExpression(
+    '=',
+    types.MemberExpression(declaration.id, types.identifier('$inject')),
+    types.arrayExpression(params.map((identifier) => types.stringLiteral(identifier.name)))
+  )));
+}
+
+module.exports = function ({ types }) {
   return {
     visitor: {
-      FunctionDeclaration: {
+      'ExportNamedDeclaration|ExportDefaultDeclaration': {
         enter(path) {
           // skip when not annotation comment
-          if (!inspectAnnotationComment(path.node)) return;
+          if (inspectAnnotationExports(path, types)) {
+            if (types.isFunctionDeclaration(path.node.declaration)) {
+              annotateFunctionDeclare(path, types);
+            }
+
+            if (types.isClassDeclaration(path.node.declaration)) {
+              annotateClassDeclare(path, types);
+            }
+          }
         }
       }
     }
